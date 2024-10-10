b16e89db4207ee43c9770d65270983cc0fa79469
Vito Zou <vzou@usc.edu>
2024-10-09 21:00:19 -0700

    implement color picker and border toggle

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 console.log("Hello Worldo!");
 const gridContainerDiv = document.getElementById("gridContainer");
+let gridSquares;
 
 function setupGrids() {
     // can set up so that the max i and j are chooseable
@@ -10,22 +11,49 @@ function setupGrids() {
         for(var j = 0; j < 16; j++) {
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
+            gridSquare.style.border = "2px solid black";
             gridRow.appendChild(gridSquare);
         }
         gridContainerDiv.appendChild(gridRow);
     }
-}
-
-function resetGrids() {
-    gridSquares.forEach((currentSquare) => {
-        currentSquare.style.backgroundColor = "aqua";
-    })
+    gridSquares = document.querySelectorAll(".grid-square");
 }
 
 setupGrids();
 
-const gridSquares = document.querySelectorAll(".grid-square");
+function resetGrids() {
+    gridSquares.forEach((currentSquare) => {
+        currentSquare.style.backgroundColor = "white";
+    });
+}
+
+function chooseColor() {
+    currentColor = colorPicker.value;
+}
+
+function toggleBorder() {
+    borderON = !borderON;
+    if(borderON) {
+        borderToggleButton.textContent = "Border ON";
+        gridSquares.forEach((currentSquare) => {
+            currentSquare.style.border = "2px solid black";
+        });
+    }
+    else {
+        borderToggleButton.textContent = "Border OFF";
+        gridSquares.forEach((currentSquare) => {
+            currentSquare.style.border = "none";
+        });
+    }
+}
+
+const resetButton = document.getElementById("reset-button");
+const colorPicker = document.getElementById("color-picker");
+const borderToggleButton = document.getElementById("border-toggle-button");
+
 let isMousePressed = false;
+let currentColor = colorPicker.value;
+let borderON = true;
 
 gridContainerDiv.addEventListener("mousedown", () => {
     isMousePressed=true;
@@ -38,11 +66,12 @@ gridContainerDiv.addEventListener("mouseup", ()=> {
 gridSquares.forEach((currentSquare) => {
     currentSquare.addEventListener("mouseenter", () => {
         if(isMousePressed) {
-            currentSquare.style.backgroundColor = "red";
+            currentSquare.style.backgroundColor = currentColor;
         }
     });
 });
 
-const resetButton = document.getElementById("reset-button");
-
 resetButton.addEventListener("click", resetGrids);
+colorPicker.addEventListener("input", chooseColor);
+borderToggleButton.addEventListener("click", toggleBorder);
+
